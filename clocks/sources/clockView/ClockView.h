1465db98d7ef1fce73489c615b5beafbcf01e8c0
iOS Fathers <ios.fathers.community@gmail.com>
2021-12-10 20:13:29 +0300

    Remove unused

--- clocks/sources/clockView/ClockView.h
+++ clocks/sources/clockView/ClockView.h
@@ -12,7 +12,6 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ClockView : UIView
 @property (strong, nonatomic) UIColor *fillColor;
 @property (strong, nonatomic) UIColor *strokeColor;
-@property (assign) CGFloat radius;
 
 - (void)setHour:(NSInteger)hours minute:(NSInteger)minutes seconds:(NSInteger)seconds animated:(BOOL)animated;
 @end
